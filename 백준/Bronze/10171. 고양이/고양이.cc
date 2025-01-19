c4966878d6a734a759dffa4fbfb46dc09258c326
minjun kim <162880508+minzzzun@users.noreply.github.com>
2025-01-19 21:32:48 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "\\    /\\";
+  cout << "\n";
+  cout << " )  ( ')";
+  cout << "\n";
+  cout << "(  /  )";
+  cout << "\n";
+  cout << " \\(__)|";
+  return 0;
+}
