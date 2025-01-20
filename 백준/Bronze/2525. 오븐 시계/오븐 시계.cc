77ac0ac54f0235dd31f3c3555d1f4db05757eddd
minjun kim <162880508+minzzzun@users.noreply.github.com>
2025-01-20 09:33:41 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+
+
+int  main(){
+  int hour,min;
+  int time;
+  cin >> hour >> min >>time;
+  hour += time/60;
+  min  += time%60;
+
+  if (min >=60){
+    hour += 1;
+    min -= 60; 
+  }
+  if(hour >=24){
+    hour -= 24;
+  }
+
+  cout << hour << " " << min ; 
+
+  return 0; 
+}
